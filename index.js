d4cde645a7f09ce7ef9de8604e5890ee1a5e982f
Patric Jansson <paddy@kth.se>
2021-02-13 19:02:03 +0100

    Fix for temp redirect?

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ let _ok = function ok(
  */
 let _redirect = function redirect(response, statusCode, url) {
   _addDefaultHeaders(response);
-  response.redirect(url, statusCode);
+  response.status(statusCode).redirect(url);
 };
 
 /**
